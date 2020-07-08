b11537286ba300ac843ceb15f97ac64cace6091c
Edgardo Avilés <eaviles@gmail.com>
2020-07-08 02:58:41 -0400

    Add a background layer

--- daito-manabe-01/src/ofApp.cpp
+++ daito-manabe-01/src/ofApp.cpp
@@ -2,26 +2,33 @@
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-    ofSetWindowShape(targetWidth * 2, targetHeight * 2);
+    ofSetWindowShape(IMAGE_WIDTH * 2, IMAGE_HEIGHT * 2);
     ofSetFrameRate(60);
     ofSetVerticalSync(true);
 
-    grabber.setDeviceID(2);
-    grabber.setup(targetWidth, targetHeight);
+    grabber.setDeviceID(1);
+    grabber.setup(IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    grayImg.allocate(targetWidth, targetHeight, OF_IMAGE_GRAYSCALE);
-    diffImg.allocate(targetWidth, targetHeight, OF_IMAGE_GRAYSCALE);
-    prevImg.allocate(targetWidth, targetHeight, OF_IMAGE_GRAYSCALE);
-    thrsImg.allocate(targetWidth, targetHeight, OF_IMAGE_GRAYSCALE);
+    grayImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
+    diffImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
+    prevImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
+    thrsImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
+    orgThrsImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
+    alphaImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_COLOR_ALPHA);
+    maskImg.load("mask.png");
 
-    palette.push_back(ofColor::red);
+    palette.push_back(ofColor::white);
     palette.push_back(ofColor::magenta);
+    palette.push_back(ofColor::red);
     palette.push_back(ofColor::yellow);
     palette.push_back(ofColor::green);
     palette.push_back(ofColor::blue);
 
-    finder.setMinArea(targetWidth * 0.5);
-    //    finder.setSortBySize(true);
+    finder.setMinArea((float)IMAGE_WIDTH * 0.35);
+
+    for (int i = 0; i < 2048; i++) {
+        fieldLines.push_back(fieldLine());
+    }
 }
 
 //--------------------------------------------------------------
@@ -39,6 +46,16 @@ void ofApp::update() {
         threshold(thrsImg, 256 * 0.5);
         thrsImg.update();
 
+        auto pxs = grayImg.getPixels();
+        for (int x = 0; x < IMAGE_WIDTH; x++) {
+            for (int y = 0; y < IMAGE_HEIGHT; y++) {
+                auto p = pxs.getColor(x, y);
+                ofColor c = ofColor(p.r, p.g, p.b, p.r > 128 ? 255 : 0);
+                alphaImg.setColor(x, y, c);
+            }
+        }
+        alphaImg.update();
+
         prevImg.setFromPixels(grayImg.getPixels());
         prevImg.update();
 
@@ -46,11 +63,11 @@ void ofApp::update() {
 
         ofPath p;
         for (int i = 0; i < finder.size(); i++) {
-            auto pl = finder.getPolyline(i).getResampledByCount(4);
+            auto pl = finder.getPolyline(i).getResampledByCount(3);
             for (int j = 0; j < pl.getVertices().size(); j++) {
                 auto v = pl.getVertices()[j];
-                float rx = ofRandom(-16, 16);
-                float ry = ofRandom(-16, 16);
+                float rx = ofRandom(-32, 32);
+                float ry = ofRandom(-32, 32);
                 if (j == 0) {
                     p.newSubPath();
                     p.moveTo(v.x + rx, v.y + ry);
@@ -62,7 +79,6 @@ void ofApp::update() {
         p.close();
         p.simplify();
         history.push_back(p);
-
         if (history.size() > palette.size()) {
             history.erase(history.begin());
         }
@@ -71,8 +87,25 @@ void ofApp::update() {
 
 //--------------------------------------------------------------
 void ofApp::draw() {
+    ofBackground(ofColor::black);
     ofSetColor(ofColor::white);
     ofScale(2.0);
+
+    for (int i = 0; i < fieldLines.size(); i++) {
+        if (i == fieldLines.size() * 0.75) {
+            maskImg.draw(0, 0);
+        }
+        fieldLine l = fieldLines[i];
+        if (l.z - l.l > -2400) {
+            ofSetLineWidth(l.w);
+            ofDrawLine(l.x, l.y, l.z, l.x, l.y, l.z - l.l);
+            fieldLines[i].z = l.z - 24;
+        } else {
+            fieldLines[i] = fieldLine();
+        }
+    }
+
+    //    cout << mouseX << " - " << (mouseX + 200) << endl;
 
     //    grabber.draw(0, 0);
     //
@@ -100,7 +133,16 @@ void ofApp::draw() {
     //    ofPopMatrix();
     //
 
-    grayImg.draw(0, 0);
+    //    ofEnableBlendMode(OF_BLENDMODE_SCREEN);
+    alphaImg.draw(0, 0);
+    //    ofDisableBlendMode();
+
+    //    ofSetLineWidth(8.0);
+    //    for (int i = 0; i < finder.size(); i++) {
+    //        auto p = finder.getPolyline(i).getSmoothed(10);
+    //        p.setClosed(true);
+    //        p.draw();
+    //    }
 
     for (int i = 0; i < history.size(); i++) {
         history[i].setColor(palette[i]);
--- daito-manabe-01/src/ofApp.h
+++ daito-manabe-01/src/ofApp.h
@@ -5,6 +5,38 @@
 
 using namespace ofxCv;
 
+#define IMAGE_WIDTH 640
+#define IMAGE_HEIGHT 480
+
+struct fieldLine {
+    float x;
+    float y;
+    float z;
+    float l;
+    float w;
+
+    fieldLine() {
+        float r = ofRandomuf();
+        if (r > 0.525)
+            x = ofRandom((float)IMAGE_WIDTH * 0.6, IMAGE_WIDTH);
+        else if (r > 0.05)
+            x = ofRandom(0, (float)IMAGE_WIDTH * 0.4);
+        else
+            x = ofRandom((float)IMAGE_WIDTH * 0.4, (float)IMAGE_WIDTH * 0.6);
+
+        y = ofRandom(0, IMAGE_HEIGHT);
+        z = ofRandom(2400, 2400 * 2);
+        //        z = ofRandom(, 1200);
+        l = ofRandom(400, 2400);
+
+        r = ofRandomuf();
+        if (r > 0.98) w = 8;
+        //        else if (r > 0.8) w = 4;
+        else
+            w = 2;
+    }
+};
+
 class ofApp : public ofBaseApp {
   public:
     void setup();
@@ -28,10 +60,12 @@ class ofApp : public ofBaseApp {
     ofImage diffImg;
     ofImage prevImg;
     ofImage thrsImg;
+    ofImage maskImg;
+    ofImage orgThrsImg;
+    ofImage alphaImg;
     vector<ofPath> history;
     ContourFinder finder;
     vector<ofColor> palette;
 
-    int targetWidth = 640;
-    int targetHeight = 480;
+    vector<fieldLine> fieldLines;
 };
