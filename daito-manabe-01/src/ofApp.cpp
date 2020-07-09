d0d6a30b054cd113d6e844c4e35e41c135980705
Edgardo Avilés <eaviles@gmail.com>
2020-07-08 21:59:42 -0400

    Add a body layer

--- daito-manabe-01/src/ofApp.cpp
+++ daito-manabe-01/src/ofApp.cpp
@@ -6,27 +6,27 @@ void ofApp::setup() {
     ofSetFrameRate(60);
     ofSetVerticalSync(true);
 
-    grabber.setDeviceID(1);
+    // grabber.setDeviceID(1);
     grabber.setup(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     grayImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
     diffImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
     prevImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
     thrsImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
-    orgThrsImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_GRAYSCALE);
-    alphaImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_COLOR_ALPHA);
+    bodyImg.allocate(IMAGE_WIDTH, IMAGE_HEIGHT, OF_IMAGE_COLOR_ALPHA);
     maskImg.load("mask.png");
 
-    palette.push_back(ofColor::white);
     palette.push_back(ofColor::magenta);
     palette.push_back(ofColor::red);
     palette.push_back(ofColor::yellow);
     palette.push_back(ofColor::green);
     palette.push_back(ofColor::blue);
+    palette.push_back(ofColor::white);
 
-    finder.setMinArea((float)IMAGE_WIDTH * 0.25);
+    motionFinder.setMinArea((float)IMAGE_WIDTH * 0.25);
+    bodyFinder.setMinArea((float)IMAGE_WIDTH * 0.25);
 
-    for (int i = 0; i < 2048; i++) {
+    for (int i = 0; i < FIELD_LINES_MAX; i++) {
         fieldLines.push_back(fieldLine());
     }
 }
@@ -34,53 +34,121 @@ void ofApp::setup() {
 //--------------------------------------------------------------
 void ofApp::update() {
     grabber.update();
+    if (!grabber.isFrameNew()) return;
 
-    if (grabber.isFrameNew()) {
-        convertColor(grabber, grayImg, CV_RGB2GRAY);
-        grayImg.update();
+    // Get a greyscale image from the camera.
+    convertColor(grabber, grayImg, CV_RGB2GRAY);
+    grayImg.update();
 
-        absdiff(grayImg, prevImg, diffImg);
-        diffImg.update();
+    // Get the diff to detect motion.
+    absdiff(grayImg, prevImg, diffImg);
+    diffImg.update();
 
-        thrsImg.setFromPixels(diffImg.getPixels());
-        threshold(thrsImg, 256 * 0.25);
-        thrsImg.update();
+    // Threshold the diff image.
+    thrsImg.setFromPixels(diffImg.getPixels());
+    threshold(thrsImg, 256 * 0.25);
+    thrsImg.update();
 
-        auto pxs = grayImg.getPixels();
-        for (int x = 0; x < IMAGE_WIDTH; x++) {
-            for (int y = 0; y < IMAGE_HEIGHT; y++) {
-                auto p = pxs.getColor(x, y);
-                ofColor c = ofColor(p.r, p.g, p.b, p.r < 160 ? 255 : 0);
-                alphaImg.setColor(x, y, c);
+    // Store the prev image.
+    prevImg.setFromPixels(grayImg.getPixels());
+    prevImg.update();
+
+    // Find the motion contours.
+    motionFinder.findContours(thrsImg);
+
+    // Convert outlines to shapes.
+    ofPath p;
+    for (int i = 0; i < motionFinder.size(); i++) {
+        auto pl = motionFinder.getPolyline(i).getResampledByCount(12);
+        for (int j = 0; j < pl.getVertices().size(); j++) {
+            auto v = pl.getVertices()[j];
+            float rx = 0;//ofRandom(-4, 4);
+            float ry = 0;//ofRandom(-4, 4);
+            if (j == 0) {
+                p.newSubPath();
+                p.moveTo(v.x + rx, v.y + ry);//, ofRandom(-12, 12));
+            } else {
+                p.lineTo(v.x + rx, v.y + ry);//, ofRandom(-12, 12));
             }
         }
-        alphaImg.update();
+    }
+    p.close();
+    p.simplify();
 
-        prevImg.setFromPixels(grayImg.getPixels());
-        prevImg.update();
+    // Maintain a history of motion shapes.
+    history.push_back(p);
+    if (history.size() > palette.size()) {
+        history.erase(history.begin());
+    }
 
-        finder.findContours(thrsImg);
+    // Threshold the camera image to get the body.
+    thrsImg.setFromPixels(grayImg.getPixels());
+    float bodyThreshold = 256 * 0.6;
+    threshold(thrsImg, bodyThreshold);
+    invert(thrsImg);
+    thrsImg.update();
 
-        ofPath p;
-        for (int i = 0; i < finder.size(); i++) {
-            auto pl = finder.getPolyline(i).getResampledByCount(12);
-            for (int j = 0; j < pl.getVertices().size(); j++) {
-                auto v = pl.getVertices()[j];
-                float rx = ofRandom(-4, 4);
-                float ry = ofRandom(-4, 4);
-                if (j == 0) {
-                    p.newSubPath();
-                    p.moveTo(v.x + rx, v.y + ry, ofRandom(-12,12));
-                } else {
-                    p.lineTo(v.x + rx, v.y + ry, ofRandom(-12,12));
-                }
-            }
+    bodyFinder.findContours(thrsImg);
+
+    auto pxls = grayImg.getPixels();
+    for (int x = 0; x < IMAGE_WIDTH; x++) {
+        for (int y = 0; y < IMAGE_HEIGHT; y++) {
+            auto c = pxls.getColor(x, y);
+            auto d = ofColor(0, 0, 0, c.r < bodyThreshold ? 255 : 0);
+            bodyImg.setColor(x, y, d);
         }
-        p.close();
-        p.simplify();
-        history.push_back(p);
-        if (history.size() > palette.size()) {
-            history.erase(history.begin());
+    }
+    bodyImg.update();
+}
+
+//------------------------------------------------------------------------------
+void ofApp::drawBackground() {
+    for (int i = 0; i < fieldLines.size(); i++) {
+        if (i == FIELD_LINES_MASK_INDEX) {
+            maskImg.draw(0, 0);
+        }
+        fieldLine l = fieldLines[i];
+        if (l.z - l.l > -FIELD_LINES_DEPTH) {
+            ofSetLineWidth(l.w);
+            ofDrawLine(l.x, l.y, l.z, l.x, l.y, l.z - l.l);
+            fieldLines[i].z = l.z - FIELD_LINES_SPEED;
+        } else {
+            fieldLines[i] = fieldLine();
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+void ofApp::drawMotion() {
+    for (int i = 0; i < history.size(); i++) {
+        history[i].setColor(palette[i]);
+        history[i].draw();
+    }
+}
+
+//------------------------------------------------------------------------------
+void ofApp::drawBody() {
+    if (bodyFinder.size() == 0) return;
+
+    for (int i = 0; i < bodyFinder.size(); i++) {
+        auto d = bodyFinder.getPolyline(i).getResampledBySpacing(24);
+        ofSetColor(ofColor::white);
+        ofSetLineWidth(10);
+        d.draw();
+
+        vector<ofPoint> pts;
+        for (int j = 0; j < d.size(); j++) {
+            pts.push_back(d[j]);
+        }
+        ofRandomize(pts);
+
+        for (int j = 1; j < pts.size(); j++) {
+            ofPoint p1 = pts[j - 1];
+            ofPoint p2 = pts[j];
+            auto d = (p1 - p2).length();
+            if (d > 160) continue;
+            ofSetLineWidth(ofRandomuf() > 0.75 ? 6 : 2);
+            ofDrawLine(p1, p2);
         }
     }
 }
@@ -91,60 +159,10 @@ void ofApp::draw() {
     ofSetColor(ofColor::white);
     ofScale(2.0);
 
-    for (int i = 0; i < fieldLines.size(); i++) {
-        if (i == fieldLines.size() * 0.75) {
-            maskImg.draw(0, 0);
-        }
-        fieldLine l = fieldLines[i];
-        if (l.z - l.l > -2400) {
-            ofSetLineWidth(l.w);
-            ofDrawLine(l.x, l.y, l.z, l.x, l.y, l.z - l.l);
-            fieldLines[i].z = l.z - 24;
-        } else {
-            fieldLines[i] = fieldLine();
-        }
-    }
-
-    //    cout << mouseX << " - " << (mouseX + 200) << endl;
-
-//        grabber.draw(0, 0);
-    //
-//        grayImg.draw(IMAGE_WIDTH, 0);
-    //
-//        diffImg.draw(0, IMAGE_HEIGHT);
-    //
-//        thrsImg.draw(IMAGE_WIDTH, IMAGE_HEIGHT);
-    //
-//        if (finder.size() > 0) {
-//            finder.draw();
-//        }
-    //
-    //    ofPushMatrix();
-    //    ofTranslate(0, 0);
-    //    for (int i = 0; i < history.size(); i++) {
-    //        ofColor c = palette[i];
-    //        c.a = 220;
-    //        history[i].setColor(c);
-    //        history[i].draw();
-    //    }
-    //    ofPopMatrix();
-    //
-
-    //    ofEnableBlendMode(OF_BLENDMODE_SCREEN);
-    alphaImg.draw(0, 0);
-    //    ofDisableBlendMode();
-
-    //    ofSetLineWidth(8.0);
-    //    for (int i = 0; i < finder.size(); i++) {
-    //        auto p = finder.getPolyline(i).getSmoothed(10);
-    //        p.setClosed(true);
-    //        p.draw();
-    //    }
-
-    for (int i = 0; i < history.size(); i++) {
-        history[i].setColor(palette[i]);
-        history[i].draw();
-    }
+    drawBackground();
+    drawMotion();
+    bodyImg.draw(0, 0);
+    drawBody();
 }
 
 //--------------------------------------------------------------
--- daito-manabe-01/src/ofApp.h
+++ daito-manabe-01/src/ofApp.h
@@ -7,6 +7,10 @@ using namespace ofxCv;
 
 #define IMAGE_WIDTH 640
 #define IMAGE_HEIGHT 480
+#define FIELD_LINES_MAX 2048
+#define FIELD_LINES_MASK_INDEX 1800
+#define FIELD_LINES_DEPTH 2400
+#define FIELD_LINES_SPEED 24
 
 struct fieldLine {
     float x;
@@ -17,23 +21,24 @@ struct fieldLine {
 
     fieldLine() {
         float r = ofRandomuf();
-        if (r > 0.525)
+        if (r > 0.525) {
             x = ofRandom((float)IMAGE_WIDTH * 0.6, IMAGE_WIDTH);
-        else if (r > 0.05)
+        } else if (r > 0.05) {
             x = ofRandom(0, (float)IMAGE_WIDTH * 0.4);
-        else
+        } else {
             x = ofRandom((float)IMAGE_WIDTH * 0.4, (float)IMAGE_WIDTH * 0.6);
+        }
 
         y = ofRandom(0, IMAGE_HEIGHT);
-        z = ofRandom(2400, 2400 * 2);
-        //        z = ofRandom(, 1200);
-        l = ofRandom(400, 2400);
-
+        z = ofRandom(FIELD_LINES_DEPTH, FIELD_LINES_DEPTH * 2);
+        l = ofRandom(FIELD_LINES_DEPTH * 0.25, FIELD_LINES_DEPTH);
         r = ofRandomuf();
-        if (r > 0.98) w = 8;
-        //        else if (r > 0.8) w = 4;
-        else
+
+        if (r > 0.98) {
+            w = 12;
+        } else {
             w = 2;
+        }
     }
 };
 
@@ -55,17 +60,21 @@ class ofApp : public ofBaseApp {
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
 
-    ofVideoGrabber grabber;
-    ofImage grayImg;
+    void drawBackground();
+    void drawMotion();
+    void drawBody();
+
     ofImage diffImg;
+    ofImage grayImg;
+    ofImage maskImg;
     ofImage prevImg;
     ofImage thrsImg;
-    ofImage maskImg;
-    ofImage orgThrsImg;
-    ofImage alphaImg;
-    vector<ofPath> history;
-    ContourFinder finder;
-    vector<ofColor> palette;
+    ofImage bodyImg;
 
+    ContourFinder motionFinder;
+    ContourFinder bodyFinder;
+    ofVideoGrabber grabber;
     vector<fieldLine> fieldLines;
+    vector<ofColor> palette;
+    vector<ofPath> history;
 };
